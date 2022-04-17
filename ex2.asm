a180e7bb0054b824efef1f8be62331ec2cc5ec21
Dan <dansdeor@campus.technion.ac.il>
2022-04-17 21:49:38 +0300

    Adding 2 & 3  asm files and dry work to the repo

--- ex2.asm
+++ ex2.asm
@@ -2,4 +2,48 @@
 
 .section .text
 _start:
-#your code here
+# your code here
+xorq %rax, %rax
+movl (num), %eax
+cmpl $0, %eax
+je exit_copy_HW1
+jg positive_num_HW1
+# number is negative:
+movq $destination, %rsi
+movq $source, %rdi
+negl %eax
+jmp overlapping_check_HW1
+positive_num_HW1:
+movq $source, %rsi
+movq $destination, %rdi
+
+# in case where some of the bytes of the buffers are being shared
+# we have to change the bytes copying method (from the beginning of the buffer or from the end)
+overlapping_check_HW1:
+cmpq %rsi, %rdi
+jge copy_from_end_prep_HW1
+
+copy_from_start_HW1:
+decl %eax
+movb (%rsi), %dl
+movb %dl, (%rdi)
+incq %rsi
+incq %rdi
+cmpl $0, %eax
+jne copy_from_start_HW1
+jmp exit_copy_HW1
+
+copy_from_end_prep_HW1:
+addq %rax, %rdi
+addq %rax, %rsi
+decq %rdi
+decq %rsi
+copy_from_end_HW1:
+decl %eax
+movb (%rsi), %dl
+movb %dl, (%rdi)
+decq %rsi
+decq %rdi
+cmpl $0, %eax
+jne copy_from_end_HW1
+exit_copy_HW1:
--- ex3.asm
+++ ex3.asm
@@ -2,4 +2,65 @@
 
 .section .text
 _start:
-#your code here
+# your code here
+movq $array1, %rsi
+movq $array2, %rdi
+movq $mergedArray, %rax
+
+pick_one_HW1:
+movl (%rsi), %ebx
+movl (%rdi), %ecx
+# when we get to one of the arrays ends we need to copy from the one left
+cmpl $0, %ebx
+je copy_from_array_left_prep_HW1
+cmpl $0, %ecx
+je copy_from_array_left_HW1
+cmpl %ebx, %ecx
+jb check_array1_dup_HW1
+ja check_array2_dup_HW1
+addq $4, %rsi
+jmp pick_one_HW1
+
+check_array1_dup_HW1:
+cmpl %ebx, 4(%rsi)
+jne copy_from_array1_HW1
+addq $4, %rsi
+jmp check_array1_dup_HW1
+
+copy_from_array1_HW1:
+movl %ebx, (%rax)
+addq $4, %rsi
+addq $4, %rax
+jmp pick_one_HW1
+
+check_array2_dup_HW1:
+cmpl %ecx, 4(%rdi)
+jne copy_from_array2_HW1
+addq $4, %rdi
+jmp check_array2_dup_HW1
+
+copy_from_array2_HW1:
+movl %ecx, (%rax)
+addq $4, %rdi
+addq $4, %rax
+jmp pick_one_HW1
+
+copy_from_array_left_prep_HW1:
+movq %rdi, %rsi
+copy_from_array_left_HW1:
+movl (%rsi), %ebx
+addq $4, %rsi
+# got to the end
+cmpl $0, %ebx
+je exit_merge_copy_HW1
+# used to remove duplicates
+cmpq $mergedArray, %rax
+je prevent_first_time_HW1
+cmpl %ebx, -4(%rax)
+je copy_from_array_left_HW1
+prevent_first_time_HW1:
+movl %ebx, (%rax)
+addq $4, %rax
+jmp copy_from_array_left_HW1
+exit_merge_copy_HW1:
+movl $0, (%rax)
